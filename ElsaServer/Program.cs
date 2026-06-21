d7b29038bd8d5880db9d1ccec5598dda0058b381
Hayden Li <mli10@woolworths.com.au>
2026-06-22 08:50:17 +1000

    Add project files.

---
+++ ElsaServer/Program.cs
@@ -0,0 +1,58 @@
+using Elsa.Extensions;
+using Elsa.Persistence.EFCore.Extensions;
+using Elsa.Persistence.EFCore.Modules.Management;
+using Elsa.Persistence.EFCore.Modules.Runtime;
+using Elsa.Workflows;
+using ElsaServer.SchedulingEngine.Workflows;
+using Microsoft.AspNetCore.Mvc;
+using SchedulingEngine.BusinessEngine.Modules;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseStaticWebAssets();
+
+var services = builder.Services;
+var configuration = builder.Configuration;
+
+services
+    .AddElsa(elsa => elsa
+        .UseIdentity(identity =>
+        {
+            identity.TokenOptions = options => options.SigningKey = "large-signing-key-for-signing-JWT-tokens";
+            identity.UseAdminUserProvider();
+        })
+        .UseDefaultAuthentication()
+        .UseWorkflowManagement(management => management.UseEntityFrameworkCore(ef => ef.UseSqlite()))
+        .UseWorkflowRuntime(runtime => runtime.UseEntityFrameworkCore(ef => ef.UseSqlite()))
+        .UseScheduling()
+        .UseJavaScript()
+        .UseLiquid()
+        .UseCSharp()
+        .UseHttp(http => http.ConfigureHttpOptions = options => configuration.GetSection("Http").Bind(options))
+        .UseWorkflowsApi()
+        .AddActivitiesFrom<Program>()
+        .AddWorkflowsFrom<Program>()
+    );
+
+services.AddCors(cors => cors.AddDefaultPolicy(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().WithExposedHeaders("*")));
+services.AddRazorPages(options => options.Conventions.ConfigureFilter(new IgnoreAntiforgeryTokenAttribute()));
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.MapStaticAssets();
+app.UseRouting();
+app.UseCors();
+app.UseStaticFiles();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseWorkflowsApi();
+app.UseWorkflows();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
